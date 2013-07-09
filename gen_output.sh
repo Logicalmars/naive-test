0ef230415cb05efe7a037873e155a34b25b8c0af
Meng Lin <bluebird498@gmail.com>
2013-07-09 16:21:47 -0700

    Update gen_output.sh

--- gen_output.sh
+++ gen_output.sh
@@ -2,6 +2,10 @@
 BASE_EXEC=cat
 FILES=test_data/*
 
+# rm the whole output folder, so we have diff error if we fail to gen output
+rm -rf output
+mkdir output
+
 echo "--------------Generating output----------------"
 for f in $FILES
 do
